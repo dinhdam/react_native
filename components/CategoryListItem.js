568c26834c1f11e94b61b66b15dc5474bcd00f14
dinhdam <dinhdam24492@gmail.com>
2019-11-19 11:38:26 +0700

    StyleSheet

---
+++ components/CategoryListItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import medicine from '../assets/medicine.png'
+import{
+    Image,
+    Text,
+    View,
+    StyleSheet
+} from 'react-native'
+// dung function components ta se dung export default
+export default function CategoryListItem() {
+    return <View style={styles.container}>
+       <Text style={styles.title}>CategoryListItem</Text>
+        <Image style={styles.categoryImage} source={medicine}/>
+    </View>
+
+
+
+}
+const  styles = StyleSheet.create({
+ categoryImage:{
+     width: 64,
+     height:64
+ },
+ container:{
+   alignItems: 'center',
+     padding:16,
+     shadowColor : '#000',
+     shadowOpacity:0.3,
+     shadowRadius:10,
+     shadowOffset:{width: 0,height: 0},
+     borderRadius:4,
+     backgroundColor:'#FFF',
+     // shadowColor: "#000",
+     // shadowOffset: {
+     //     width: 0,
+     //     height: 2,
+     // },
+     // shadowOpacity: 0.25,
+     // shadowRadius: 3.84,
+
+     elevation: 5,
+     marginBottom: 16
+
+
+ },
+ title:{
+     textTransform : 'uppercase',
+     marginBottom:8,
+     fontWeight :'700'
+
+ }
+});
+
+
